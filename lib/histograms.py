dba241c254e4e5c9c0342d71648f5c41b8e7a8b4
Andrew Carnes <carnesandnoble@gmail.com>
2016-04-26 12:56:30 -0400

    Fixed histogram plotting to use df.values rather than the dataframes themselves.

--- lib/histograms.py
+++ lib/histograms.py
@@ -95,7 +95,7 @@ def histogram_2d(x, y, axis=0, bins=(25,25), maxvalues=42):
         ymax = ymax+1
 
     # Use numpy to make the 2D histogram then plot the histogram appropriately
-    hist,xedges,yedges = np.histogram2d(y,x,bins=bins)
+    hist,xedges,yedges = np.histogram2d(y.values,x.values,bins=bins)
     color_bar=0
 
     # plot the histogram on a certain axis
@@ -210,7 +210,7 @@ def p_y_given_x(x, y, axis=0, bins=(25,25), maxvalues=42):
         ymax = ymax+1
 
     # Use numpy to make the 2D histogram then plot the histogram appropriately
-    hist,xedges,yedges = np.histogram2d(y,x,bins=bins)
+    hist,xedges,yedges = np.histogram2d(y.values,x.values,bins=bins)
 
     # Here we form p_y_given_x by dividing n_x_y by n_x
     normalized = pd.DataFrame(hist)
@@ -247,6 +247,72 @@ def p_y_given_x_all_vars(df, target, bins=(25,25), maxvalues=42):
         current_axis = fig.add_subplot(5, 3, i)
         current_axis.set_title(col)
         p_y_given_x(df[col], target, axis=current_axis, bins=bins, maxvalues=maxvalues)
+        i+=1
+    fig.tight_layout()
+    return fig
+
+#------------------------------------------------
+# ===============================================
+#------------------------------------------------
+
+def p_x_given_y(x, y, axis=0, bins=(25,25), maxvalues=42):
+# Make a 2d histogram from the columns, x,y, of a data frame
+
+    xmin = x.min()
+    xmax = x.max()
+    ymin = y.min()
+    ymax = y.max()
+
+    # The number of unique values in each vector
+    xcounts = x.value_counts().size
+    ycounts = y.value_counts().size
+
+    # If there are not many unique values we can have a bin per value
+    if xcounts < maxvalues:
+        bins = (bins[0], xcounts)
+        xmax = xmax+1
+    if ycounts < maxvalues:
+        bins = (ycounts, bins[1]) 
+        ymax = ymax+1
+
+    # Use numpy to make the 2D histogram then plot the histogram appropriately
+    hist,xedges,yedges = np.histogram2d(y.values,x.values,bins=bins)
+
+    # Here we form p_x_given_y by dividing n_x_y by n_y
+    normalized = pd.DataFrame(hist)
+    normalized = normalized.div(normalized.sum(axis=1), axis=0)
+    color_bar=0
+
+    # plot the histogram on a certain axis
+    if axis!=0:
+        color_bar = axis.imshow(normalized, extent=[xmin,xmax,ymin,ymax], interpolation='nearest', aspect='auto', origin='lower')
+        axis.grid(1)
+        axis.set_xlabel(x.name)
+        axis.set_ylabel(y.name)
+
+    # plot the histogram through the main plotting system
+    else:
+        color_bar = plt.imshow(normalized, extent=[xmin,xmax,ymin,ymax], interpolation='nearest', aspect='auto', origin='lower')
+        plt.grid(1)
+        plt.xlabel(x.name)
+        plt.ylabel(y.name)
+
+#------------------------------------------------
+# ===============================================
+#------------------------------------------------
+
+def p_x_given_y_all_vars(df, target, bins=(25,25), maxvalues=42): 
+    # Keep track of the subplot we are in
+    i=1
+    
+    # Make a figure and declare the size
+    fig = plt.figure(figsize=(15, 12))
+
+    # Make histograms for each column and put them in the figure
+    for col in df.columns:
+        current_axis = fig.add_subplot(5, 3, i)
+        current_axis.set_title(col)
+        p_x_given_y(df[col], target, axis=current_axis, bins=bins, maxvalues=maxvalues)
         i+=1
     fig.tight_layout()
     return fig
@@ -422,7 +488,7 @@ def histogram_2d_xyz(x, y, z, axis=0, bins=(25,25), maxvalues=42, ztype='median'
         ymax = ymax+1
 
     # Use numpy to make the 2D histogram. This will have the count in each bin but we want some version of the z value. 
-    hist,xedges,yedges = np.histogram2d(y,x,bins=bins)
+    hist,xedges,yedges = np.histogram2d(y.values,x.values,bins=bins)
 
     # Let's get the z value
     
--- santander/plot.py
+++ santander/plot.py
@@ -25,9 +25,17 @@ mi = histos.mutual_information(df, df["TARGET"], bins=[25,25])
 
 # get a dataframe of the top 15 mutual info variables 
 dftopv = pd.DataFrame()
+dflastv = pd.DataFrame()
 dftopv = df[list(mi[:15].index)]
+dflastv = df[list(mi[-15:].index)]
 
-histos.histogram_all_vars(dftopv);
+#histos.histogram_all_vars(dftopv)
+#histos.p_y_given_x_all_vars(dftopv, dftopv['TARGET'])
+#histos.p_x_given_y_all_vars(dftopv, dftopv['TARGET'])
+histos.p_y_given_x_all_vars(dflastv, dftopv['TARGET'])
+#histos.p_x_given_y_all_vars(dflastv, dftopv['TARGET'])
+
+plt.show()
 
 #df.ix[:5,0:10] # row begin:row end, column begin:column end
 #num_unique_values = col.value_counts().size
